edef927d9cbb2611a3eea7088400d6df82c4cace
Glitch (lead-spike) <none>
2019-02-15 22:53:01 +0000

    🍛📰 Checkpoint ./server.js:2398890/1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date_string?", function (req, res) {
+app.get("/api/timestamp/:date_string", function (req, res) {
   var dateString = req.query.date_string;
   var unixDate = Date.now();
   var utcDate = new Date().toUTCString();
