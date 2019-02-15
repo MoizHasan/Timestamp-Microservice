146156c8cbc91dd58395bf9408ccc6e12d8ef753
Glitch (lead-spike) <none>
2019-02-15 04:26:52 +0000

    🚔🛍 Checkpoint ./myapp.js:2398890/151 ./server.js:2398890/102

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  res.  
+});
+
 
 
 // listen for requests :)
